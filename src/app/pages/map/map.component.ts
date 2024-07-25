4e9463c7923f9134741c54c09139612de41beae3
Lautaro Castellanos <laumol159@gmail.com>
2024-07-25 16:34:46 -0300

    terminado

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
-import { marker, tileLayer } from 'leaflet';
-import { json } from 'stream/consumers';
+
 
 @Component({
   selector: 'app-map',
@@ -19,20 +18,45 @@ export class MapComponent implements OnInit, AfterViewInit {
   constructor(private placeSvc: PlacesService) {}
 
   Reload() {
+    localStorage.removeItem('geoLoc')
     location.reload();
   }
-
+  //En la funcion locate se ejemplifican algunos metodos para usar con el marcador
   Locate() {
     if (this.map && this.geo) {
       import('leaflet').then(({ marker }) => {
         this.ubi = localStorage.getItem('geoLoc');
-        const miUbi = marker(JSON.parse(this.ubi)).addTo(this.map).bindPopup("<b>Esta es su ubicacion</b>").openPopup().openPopup();
+        const miUbi = marker(JSON.parse(this.ubi),{draggable:true}).addTo(this.map).bindPopup(`<b>Esta es la ubicacion: </b> ${this.counter}`).openPopup();
+        let newUbi = miUbi.setLatLng(JSON.parse(this.ubi));
+
+        console.log(newUbi);
+
+        miUbi.on('click',()=> console.log('Presione click'));
+        
+        miUbi.on('dblclick',()=> console.log('Presione doble click'));
+
+        miUbi.on('moveend',()=>{
+          let moveUbi = Object.values(newUbi);
+          let meUbi = [moveUbi[1].lat,moveUbi[1].lng];
+
+          this.ubi = meUbi;
+
+          console.log(newUbi);
+          console.log(meUbi);
+
+          localStorage.setItem('geoLoc',JSON.stringify(this.ubi));
+        });
+
+        this.counter++;
+        this.map.flyTo([miUbi.getLatLng().lat,miUbi.getLatLng().lng]);
+
       }).catch(err => console.error('Error loading Leaflet marker:', err));
     }
     
   }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.counter = 0;
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
       document.addEventListener('userLocationReady', () => {
         this.initializeMap();
